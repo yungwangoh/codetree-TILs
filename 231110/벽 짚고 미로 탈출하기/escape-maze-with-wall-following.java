a548181674cc406ea028b72710e542767e320200
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-10 23:15:46 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231110/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static char[][] arr;
+    static int[] dy = {1, 0, -1, 0};
+    static int[] dx = {0, 1, 0, -1};
+    static int x;
+    static int y;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        x = sc.nextInt() - 1;
+        y = sc.nextInt() - 1;
+
+        arr = new char[n][n];
+        int max = 0;
+        for(int i = 0; i < n; i++) {
+            String s = sc.next();
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = s.charAt(j);
+
+                if(arr[i][j] == '.') max++;
+            }
+        }
+
+        int cnt = 0;
+        int dir = 0;
+        while(max-- > 0) {
+            if(isRange(x, y, n)) break;
+
+            if(check(dir, '#', n) && check(dir + 1, '#', n)) dir = (dir + 3) % 4;
+            else if(check((dir + 2) % 4, '@', n) && check((dir + 1) % 4, '.', n)) dir = (dir + 1) % 4;
+
+            move(dir);
+            cnt++;
+
+            //System.out.println(x + " " + y);
+        }
+
+        if(cnt >= max) {
+            System.out.println(-1);
+        } else {
+            System.out.println(cnt);
+        }
+    }
+    static boolean isRange(int x, int y, int n) {
+        return x < 0 || y < 0 || x >= n || y >= n;
+    }
+    static boolean check(int dir, char ch, int n) {
+
+        int nx = x + dx[dir];
+        int ny = y + dy[dir];
+
+        if(isRange(nx, ny, n)) return false;
+
+        if(arr[nx][ny] == ch) return true;
+        
+        return false;
+    }
+    static void move(int dir) {
+        arr[x][y] = '@';
+        x += dx[dir];
+        y += dy[dir];
+    }
+}

c5e29b4a8e14cdd4131c5439672c67d6d2ac84bf
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-17 23:11:50 +0900

    Title: 아름다운 수, Time: 253ms, Memory: 12MB, Status: Passed - CodeTree

--- 231117/아름다운 수/beautiful-number.java
+++ 231117/아름다운 수/beautiful-number.java
@@ -4,18 +4,18 @@ import java.util.*;
 public class Main {
 
     static List<Integer> list = new ArrayList<>();
-    static int cnt = 0;
+    static int cnt = 0, n;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        n = sc.nextInt();
 
-        search(0, n);
+        search(0);
 
         System.out.println(cnt);
     }
-    static void search(int idx, int n) {
+    static void search(int idx) {
         if(idx == n) {
             if(check()) cnt++;
             return;
@@ -23,11 +23,21 @@ public class Main {
         
         for(int i = 1; i <= 4; i++) {
             list.add(i);
-            search(idx + 1, n);
+            search(idx + 1);
             list.remove(list.size() - 1);
         }
     }
     static boolean check() {
-        
+
+        for(int i = 0; i < n; i += list.get(i)) {
+
+            if(i + list.get(i) - 1 >= n)
+                return false;
+
+            for(int j = i; j < i + list.get(i); j++)
+                if(list.get(j) != list.get(i))
+                    return false;
+        }
+        return true;
     }
 }

6820645abf6db0469b33baa45eaf8277872e4ee5
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-19 18:38:33 +0900

    Title: 겹치지 않게 선분 고르기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231119/겹치지 않게 선분 고르기/select-segments-without-overlap.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+class Pair {
+
+    int x1, x2;
+
+    public Pair(int x1, int x2) {
+        this.x1 = x1;
+        this.x2 = x2;
+    }
+}
+public class Main {
+
+    static List<Pair> list = new ArrayList<>();
+    static List<Pair> nList = new ArrayList<>();
+    static int n, max = 0;
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+
+            list.add(new Pair(x1, x2));
+        }
+
+        combi(0);
+
+        //System.out.println(check(list.get(1), list.get(2)));
+
+        System.out.println(max);        
+    }
+    static void combi(int idx) {
+        if(idx == n) {
+            int cnt = 0;
+            for(int i = 0; i < nList.size(); i++) {
+                for(int j = i + 1; j < nList.size(); j++) {
+                    if(check(nList.get(i), nList.get(j))) {
+                        cnt++;
+                    }
+                }
+            }
+            
+            max = Math.max(max, cnt);
+            return;
+        }
+
+        for(int i = 0; i < n; i++) {
+            nList.add(list.get(i));
+            combi(idx + 1);
+            nList.remove(nList.size() - 1);
+        }
+    }
+    static boolean check(Pair l1, Pair l2) {
+        int x1 = l1.x1;
+        int y1 = l1.x2;
+
+        int x2 = l2.x1;
+        int y2 = l2.x2;
+
+        return !((x1 <= x2 && x2 <= y1) || (x1 <= y2 && y2 <= y1));
+    }
+}

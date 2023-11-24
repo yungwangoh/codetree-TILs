b45d72dfa6618dba66482f385c2d5e4b68123751
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-24 23:25:46 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 138ms, Memory: 12MB, Status: Passed - CodeTree

--- 231124/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
+++ 231124/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.java
@@ -5,34 +5,48 @@ public class Main {
     
     static final int MAX = 10;
     static int n, min = Integer.MAX_VALUE;
-    static int[] a = new int[MAX];
-    static int[] b = new int[MAX];
+    static int[] a = new int[MAX * 2 + 1];
     static List<Integer> list = new ArrayList<>();
+    static int max_sum = 0;
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < 2 * n; i++) {
             a[i] = sc.nextInt();
+            max_sum += a[i];
         }
 
-        for(int i = 0; i < n; i++) {
-            b[i] = sc.nextInt();
-        }
+        func(0, 0);
 
-        func(0);
+        System.out.println(min);
     }
-    static void func(int idx) {
-        if(idx == n) {
+    static void func(int idx, int cnt) {
+        if(idx == 2 * n) {
+            if(cnt == n) {
+                min = Math.min(min, calculate());
+            }
             return;
         }
 
-        list
+        list.add(a[idx]);
+        func(idx + 1, cnt + 1);
+        list.remove(list.size() - 1);
+
+        func(idx + 1, cnt);
     }
     static int calculate() {
 
-        
+        int s1 = 0;
+        int s2 = 0;
+        for(int i = 0; i < list.size(); i++) {
+            s1 += list.get(i);
+        }
+
+        s2 = max_sum - s1;
+
+        return Math.abs(s1 - s2);
     }
 }

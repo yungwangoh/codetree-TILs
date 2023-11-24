a7ac93507ffbf2602f1f20e2db9785b16a51ca7a
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-24 15:44:43 +0900

    Title: xor 결과 최대 만들기, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231124/xor 결과 최대 만들기/max-of-xor.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m, max = Integer.MIN_VALUE;
+    static final int MX = 20;
+    static int[] arr = new int[MX];
+    static List<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
+
+        func(0, 0);
+
+        System.out.println(max);
+    }
+    static void func(int idx, int cnt) {
+        if(idx == n) {
+            if(cnt == m) {
+                max = Math.max(max, xor());
+            }
+            return;
+        }
+
+        list.add(arr[idx]);
+        func(idx + 1, cnt + 1);
+        list.remove(list.size() - 1);
+
+        func(idx + 1, cnt);
+    }
+    static int xor() {
+
+        int sum = list.get(0);
+        for(int i = 1; i < list.size(); i++) {
+            sum ^= list.get(i);
+        }
+
+        return sum;
+    }
+}

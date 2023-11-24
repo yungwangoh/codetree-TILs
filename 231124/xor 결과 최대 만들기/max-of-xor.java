3d6b3cd276ffa4bf118bda6e8f571e9d22a315c3
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-24 15:51:34 +0900

    Title: xor 결과 최대 만들기, Time: 253ms, Memory: 21MB, Status: Passed - CodeTree

--- 231124/xor 결과 최대 만들기/max-of-xor.java
+++ 231124/xor 결과 최대 만들기/max-of-xor.java
@@ -15,7 +15,7 @@ public class Main {
         m = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
-            arr[i] = i + 1;
+            arr[i] = sc.nextInt();
         }
 
         func(0, 0);

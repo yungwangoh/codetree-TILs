cb6b9f774cec72d1bfb4465110aac81389f6ed90
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-25 16:42:48 +0900

    Title: 거꾸로 순열, Time: 1344ms, Memory: 41MB, Status: Passed - CodeTree

---
+++ 231125/거꾸로 순열/backward-permutation.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 8;
+    static int n;
+    static List<Integer> list = new ArrayList<>();
+    static boolean[] visit = new boolean[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        func(0);
+    }   
+    static void func(int idx) {
+        if(idx == n) {
+            
+            for(int i = 0; i < list.size(); i++) {
+                System.out.print(list.get(i) + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = n; i >= 1; i--) {
+            if(visit[i]) continue;
+
+            visit[i] = true;
+            list.add(i);
+            func(idx + 1);
+            list.remove(list.size() - 1);
+            visit[i] = false;
+        }
+    }
+}

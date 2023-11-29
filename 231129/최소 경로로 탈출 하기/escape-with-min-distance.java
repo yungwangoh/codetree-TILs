ebda376f1d06a5904143a6c02cffe1ecad2fdfce
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-29 15:57:18 +0900

    Title: 최소 경로로 탈출 하기, Time: 364ms, Memory: 21MB, Status: Passed - CodeTree

--- 231129/최소 경로로 탈출 하기/escape-with-min-distance.java
+++ 231129/최소 경로로 탈출 하기/escape-with-min-distance.java
@@ -25,7 +25,7 @@ public class Main {
         m = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
+            for(int j = 0; j < m; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }

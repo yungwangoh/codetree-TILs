3eb8f8983d48794b794bf44447ce2ff8b8e042cf
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-30 17:55:15 +0900

    Title: 사각형 채우기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231130/사각형 채우기/rectangle-fill.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 1000;
+    static int[] memo = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i <= n; i++) memo[i] = -1;
+
+        System.out.println(dp(n));
+    }
+    static int dp(int n) {
+        if(memo[n] != -1) return memo[n];
+
+        if(n == 0) memo[n] = 0;
+        else if(n == 1) memo[n] = 1;
+        else if(n == 2) memo[n] = 2;
+        else {
+            memo[n] = (dp(n - 1) + dp(n - 2)) % 10007;
+        }
+
+        return memo[n];
+    }
+}

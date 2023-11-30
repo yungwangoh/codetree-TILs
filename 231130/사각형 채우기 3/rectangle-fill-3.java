54743d4deecd71adb16f8ea827a99d9f6699a46a
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-11-30 18:45:05 +0900

    Title: 사각형 채우기 3, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231130/사각형 채우기 3/rectangle-fill-3.java
+++ 231130/사각형 채우기 3/rectangle-fill-3.java
@@ -5,7 +5,7 @@ public class Main {
 
     static final int MAX = 1000;
     static final int MOD = 1000000007;
-    static int[] memo = new int[MAX + 1];
+    static long[] memo = new long[MAX + 1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -16,13 +16,15 @@ public class Main {
 
         System.out.println(dp(n));
     }
-    static int dp(int n) {
+    static long dp(int n) {
         if(memo[n] != -1) return memo[n];
 
         if(n == 0) memo[n] = 1;
         else if(n == 1) memo[n] = 2;
         else if(n == 2) memo[n] = 7;
-        else memo[n] = ((3 * dp(n - 1)) + dp(n - 2) - dp(n - 3)) % MOD;
+        else {
+            memo[n] = ((3 * dp(n - 1)) + dp(n - 2) - dp(n - 3)) % MOD;
+        }
 
         return memo[n];
     }

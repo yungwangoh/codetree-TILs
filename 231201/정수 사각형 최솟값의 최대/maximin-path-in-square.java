0f205e0a914c53d7dbf23de3608bd5870e717767
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-01 18:26:14 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 472ms, Memory: 23MB, Status: Passed - CodeTree

--- 231201/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 231201/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -24,19 +24,13 @@ public class Main {
 
         dp[0][0] = arr[0][0];
 
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++) {
-            min = Math.min(min, arr[0][i]);
+        for(int i = 1; i < n; i++) {
+            dp[0][i] = Math.min(dp[0][i - 1], arr[0][i]);
         }
 
-        for(int i = 1; i < n; i++) dp[0][i] = min;
-
-        min = Integer.MAX_VALUE;
-        for(int i = 0; i < n; i++) {
-            min = Math.min(min, arr[i][0]);
+        for(int i = 1; i < n; i++) {
+            dp[i][0] = Math.min(dp[i - 1][0], arr[i][0]);
         }
-
-        for(int i = 1; i < n; i++) dp[i][0] = min;
     }
     static int func(int n) {
 

ed9c700f8c4bd89c4532de6b01cc1aeb1b7aa1f2
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-01 17:52:54 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231201/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 100;
+    static int[][] arr = new int[MAX + 1][MAX + 1];
+    static int[][] dp = new int[MAX + 1][MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println(func(n));
+    }
+    static void init(int n) {
+        for(int i = 0; i < n; i++) dp[0][i] = arr[0][i];
+        for(int i = 1; i < n; i++) dp[i][0] = arr[i][0];
+    }
+    static int func(int n) {
+
+        init(n);
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(Math.min(dp[i - 1][j], arr[i][j]), Math.min(dp[i][j - 1], arr[i][j]));
+            }
+        }
+
+        return dp[n - 1][n - 1];
+    }
+}

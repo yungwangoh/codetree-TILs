96c886894bc337747ba511e1b14e2c413fb24497
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-02 17:19:18 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 338ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 231202/정수 사각형 최댓값의 최소/minimax-path-in-square.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 100;
+    static int[][] arr = new int[MAX + 1][MAX + 1];
+    static int[][] dp = new int[MAX + 1][MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        
+        System.out.println(func(n));
+        //print(n);
+    }
+    static void print(int n) {
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    static void init(int n) {
+
+        dp[0][0] = arr[0][0];
+
+        for(int i = 1; i < n; i++) dp[0][i] = Math.max(dp[0][i - 1], arr[0][i]);
+        for(int i = 1; i < n; i++) dp[i][0] = Math.max(dp[i - 1][0], arr[i][0]);
+    }
+    static int func(int n) {
+
+        init(n);
+
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(Math.min(dp[i - 1][j], dp[i][j - 1]), arr[i][j]);
+            }
+        }
+
+        return dp[n - 1][n - 1];
+    }
+}

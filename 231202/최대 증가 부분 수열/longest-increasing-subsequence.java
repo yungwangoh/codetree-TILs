f24c87475c391feda400241591326e208e631297
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-02 18:27:21 +0900

    Title: 최대 증가 부분 수열, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231202/최대 증가 부분 수열/longest-increasing-subsequence.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static final int MAX = 1000;
+    static final int INT_MIN = Integer.MIN_VALUE;
+    static int[] arr = new int[MAX + 1];
+    static int[] dp = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(func(n));
+        //print(n);
+    }
+    static void print(int n) {
+
+        for(int i = 0; i < n; i++) System.out.print(dp[i] + " ");
+        System.out.println();
+    }
+    static void init(int n) {
+        for(int i = 0; i < n; i++) dp[i] = INT_MIN;
+    }
+    static int func(int n) {
+
+        dp[0] = 1;
+
+        int max = 0;
+        for(int i = 1; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                
+                if(arr[i] > arr[j]) {
+                    max = Math.max(max, dp[j] + 1);
+                    dp[i] = max;
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}

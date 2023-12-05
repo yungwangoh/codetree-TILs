3d10235427730e6d6a591a23bc905b31917cc36f
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-05 15:47:44 +0900

    Title: 동전 거슬러주기, Time: 193ms, Memory: 10MB, Status: Passed - CodeTree

--- 231205/동전 거슬러주기/coin-change.java
+++ 231205/동전 거슬러주기/coin-change.java
@@ -7,7 +7,7 @@ public class Main {
     static final int MAX = 100;
     static final int INT = Integer.MAX_VALUE;
     static int[] arr = new int[MAX + 1];
-    static int[] dp = new int[MAX + 1];
+    static int[] dp = new int[10001];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);

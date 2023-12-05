794405f99d9630d20c955e5046c815e7c5ffbb4f
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-05 15:45:16 +0900

    Title: 동전 거슬러주기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231205/동전 거슬러주기/coin-change.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static final int MAX = 100;
+    static final int INT = Integer.MAX_VALUE;
+    static int[] arr = new int[MAX + 1];
+    static int[] dp = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println(func());
+    }
+    static void init() {
+
+        for(int i = 0; i <= m; i++) {
+            dp[i] = INT;
+        }
+
+        dp[0] = 0;
+    }
+    static int func() {
+
+        init();
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 0; j < n; j++) {
+                
+                if(i >= arr[j]) {
+                    dp[i] = Math.min(dp[i], dp[i - arr[j]] + 1);
+                }
+            }
+        }
+
+        return dp[m];
+    }
+}

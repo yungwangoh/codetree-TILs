af17a70e41cb50753a5a2c35cb8b8610438c4547
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-08 17:15:40 +0900

    Title: 두 수의 합, Time: 969ms, Memory: 54MB, Status: Passed - CodeTree

---
+++ 231208/두 수의 합/sum-of-two-num.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, k;
+    static final int MAX = 100000;
+    static long[] arr = new long[MAX + 1];
+    static Map<Long, Integer> map = new HashMap<>();
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            arr[i] = num;
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            long diff = k - arr[i];
+
+            if(map.containsKey(diff)) {
+                ans += map.get(diff);
+            }
+
+            if(!map.containsKey(arr[i])) {
+                map.put(arr[i], 1);
+            } else {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }
+        }
+
+        System.out.println(ans);
+    }
+}

dd0080327e6db0c3206a3fcaa39ff5f14e3f62c1
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-09 18:18:21 +0900

    Title: 자리 바꾸기 2, Time: 2477ms, Memory: 126MB, Status: Passed - CodeTree

--- 231209/자리 바꾸기 2/changing-seats-2.java
+++ 231209/자리 바꾸기 2/changing-seats-2.java
@@ -53,7 +53,7 @@ public class Main {
         }
 
         for(int i = 1; i <= n; i++) {
-            System.out.println(ans[i]);
+            System.out.println(set[i].size());
         }
     }
 }

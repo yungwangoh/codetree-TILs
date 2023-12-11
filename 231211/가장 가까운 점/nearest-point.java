28cbbeddd8a30616bd76eaed4f42bbfefbe2b3ad
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-11 16:17:18 +0900

    Title: 가장 가까운 점, Time: 735ms, Memory: 35MB, Status: Wrong Answer - CodeTree

---
+++ 231211/가장 가까운 점/nearest-point.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Pair implements Comparable<Pair> {
+    int x, y;
+
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Pair p) {
+        return (Math.abs(x) + Math.abs(y)) - (Math.abs(p.x) + Math.abs(p.y));
+    }
+}
+public class Main {
+
+    static int n, m;
+    static Queue<Pair> q = new PriorityQueue<>();
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            q.add(new Pair(x, y));
+        }
+
+        for(int i = 0; i < n; i++) {
+            Pair p = q.poll();
+
+            Pair np = new Pair(p.x + 2, p.y + 2);
+
+            q.add(np);
+        }
+
+        Pair p = q.peek();
+        System.out.println(p.x + " " + p.y);
+    }
+}

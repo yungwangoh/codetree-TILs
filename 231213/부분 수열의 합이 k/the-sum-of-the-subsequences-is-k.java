0fd847ae0c3d57f440d51fc0dbfc0c006cb1d71e
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-13 17:11:45 +0900

    Title: 부분 수열의 합이 k, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231213/부분 수열의 합이 k/the-sum-of-the-subsequences-is-k.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, k;
+    static final int MAX = 1000;
+    static int[] arr = new int[MAX + 1];
+    static int[] prefix = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        
+        n = sc.nextInt();
+        k = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) arr[i] = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) prefix[i] = prefix[i - 1] + arr[i];
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= n; j++) {
+                int sum = prefix[j + i - 1] - prefix[j - 1];
+
+                if(sum == k) cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}

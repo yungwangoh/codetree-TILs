03885b05be4e145f4a2d3a971df1dded7f8c21a7
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-16 17:46:19 +0900

    Title: 0에 가장 가까운 합, Time: 965ms, Memory: 34MB, Status: Wrong Answer - CodeTree

---
+++ 231216/0에 가장 가까운 합/sum-closest-to-zero.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static final int MAX = 100000;
+    static int[] arr = new int[MAX + 1];
+    static TreeSet<Integer> set = new TreeSet<>();
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) arr[i] = sc.nextInt();
+
+        Arrays.sort(arr, 1, n + 1);
+
+        int j = n;
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+
+            while(j != 1 && arr[i] + arr[j] != 0) {
+                set.add(Math.abs(arr[i] + arr[j]));
+                j--;
+            }
+
+            if(j <= i) break;
+        }
+
+        System.out.println(set.higher(0));
+    }
+}

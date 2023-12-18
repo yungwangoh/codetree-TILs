0efa19dc6975836551c856bc5700dd298f6ae65f
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-18 17:50:02 +0900

    Title: 가장 먼저 나오는 숫자, Time: 112ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/가장 먼저 나오는 숫자/first-appear-number.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static final int MAX = 100000;
+    static int[] arr = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < m; i++) {
+            int num = sc.nextInt();
+
+            int nn = lowerBound(num);
+            
+            if(nn == n - 1) System.out.println(-1);
+            else System.out.println(nn);
+        }
+    }
+    static int lowerBound(int num) {
+
+        int left = 0;
+        int right = n - 1;
+        int min = n;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] >= num) {
+                right = mid - 1;
+                min = Math.min(min, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return min + 1;
+    }
+}

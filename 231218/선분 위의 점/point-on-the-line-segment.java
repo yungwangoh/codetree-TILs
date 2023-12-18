1028642d673d3c5880a6e24d97482b7cfb08df96
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-18 18:43:19 +0900

    Title: 선분 위의 점, Time: 498ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 231218/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int n, m;
+    static final int MAX = 100000;
+    static int[] arr = new int[MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            int up = lowerBound(x);
+            int down = customLowerBound(y);
+
+            //System.out.println(up + " " + down);
+
+            if((up >= 0 && up < n) && (down >= 0 && down < n)) {
+                System.out.println(down - up + 1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+    static int lowerBound(int num) {
+
+        int left = 0;
+        int right = n - 1;
+        int min = n;
+        boolean flag = false;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] >= num) {
+                right = mid - 1;
+                min = Math.min(min, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return min;
+    }
+    static int customLowerBound(int num) {
+
+        int left = 0;
+        int right = n - 1;
+        int max = -1;
+        boolean flag = false;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] <= num) {
+                left = mid + 1;
+                max = Math.max(max, mid);
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return max;
+    }
+}

58072fbd63bb384cdce6ddf9d64b3a4d6e377bb3
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-25 16:40:42 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 714ms, Memory: 34MB, Status: Passed - CodeTree

--- 231225/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
+++ 231225/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.java
@@ -20,17 +20,15 @@ public class Main {
         int sum = 0;
         for(int i = 0; i < n; i++) {
 
-            sum += arr[i];
-
             if(sum < 0) {
-                sum = 0;
-                continue;
-            } 
+                sum = arr[i];
+            } else {
+                sum += arr[i];
+            }
 
             max = Math.max(max, sum);
         }
 
-        if(max == Integer.MIN_VALUE) max = arr[n - 1];
         System.out.println(max);
     }
 }

c50131b6edf489c73791ed37cd364899225c07ce
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-26 16:54:52 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 797ms, Memory: 36MB, Status: Passed - CodeTree

--- 231226/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
+++ 231226/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.java
@@ -1,25 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-class Pair implements Comparable<Pair> {
-
-    int n, v;
-
-    public Pair(int n, int v) {
-        this.n = n;
-        this.v = v;
-    }
-
-    @Override
-    public int compareTo(Pair p) {
-        return v - p.v;
-    }
-}
 public class Main {
     
     static int n;
     static final int MAX = 100000;
-    static Pair[] arr = new Pair[MAX + 1];
+    static int[] arr = new int[MAX + 1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
@@ -27,23 +13,17 @@ public class Main {
         n = sc.nextInt();
 
         for(int i = 0; i < n; i++) {
-            int v = sc.nextInt();
-            arr[i] = new Pair(i, v); 
+            arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(arr, 0, n);
-
         int max = 0;
-        int j = 0;
-        int sum = 0;
+        int min = arr[0];
         for(int i = 0; i < n; i++) {
+            int profit = arr[i] - min;
 
-            while(j + 1 <= n && arr[i].n <= arr[j].n) {
-                sum = arr[j].v - arr[i].v;
-                j++;
-            }
+            max = Math.max(max, profit);
 
-            max = Math.max(max, sum);
+            min = Math.min(min, arr[i]);
         }
 
         System.out.println(max);

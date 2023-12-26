c651833db2b6b349c4037d8e2b98d0e88e197c16
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2023-12-26 15:57:46 +0900

    Title: 수 채우기, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 231226/수 채우기/fill-in-number.java
+++ 231226/수 채우기/fill-in-number.java
@@ -4,21 +4,24 @@ import java.util.*;
 public class Main {
 
     static int n;
-    static int[] arr = {5, 2};
+    static final int MAX = 100000;
+    static int[] memo = new int[MAX + 1];
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
         n = sc.nextInt();
 
-        int cnt = 0;
-        for(int i = 0; i < 2; i++) {
-            cnt += (n / arr[i]);
-
-            n %= arr[i];
+        int ans = MAX;
+        for(int i = 0; i <= MAX; i++) {
+            int cnt = n - 5 * i;
+            if(cnt >= 0 && cnt % 2 == 0) {
+                ans = Math.min(ans, i + (cnt / 2));
+            }
         }
 
-        if(n == 1) cnt = -1;
-        System.out.println(cnt);
+        if(ans == MAX) ans = -1;
+
+        System.out.println(ans);
     }
 }

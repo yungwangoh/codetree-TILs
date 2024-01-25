a41d5e6ed3a1f9a69041bf7fef074eef953b0da5
윤광오 <37898720+yungwangoh@users.noreply.github.com>
2024-01-25 17:11:48 +0900

    Title: 각 정점까지의 최단 경로 2, Time: 670ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 240125/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int n, m;
+    static final int MAX = 100;
+    static int[][] arr = new int[MAX + 1][MAX + 1];
+    static int[][] dist = new int[MAX + 1][MAX + 1];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                dist[i][j] = (int)1e9;
+            }
+            dist[i][i] = 0;
+        }
+
+        for(int i = 0; i < m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+
+            if(dist[x][y] == (int)1e9) dist[x][y] = z;
+        }
+
+        for(int k = 1; k <= n; k++) {
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(dist[i][j] == (int)1e9) System.out.print(-1 + " ");
+                else System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
